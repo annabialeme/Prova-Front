5ad6b5cd275f93da934d96768b979e92a9061804
anna leme <annaleme.tds1.senai@gmail.com>
2025-05-22 11:57:22 -0300

    first commit

---
+++ src/app/home/page.jsx
@@ -0,0 +1,34 @@
+"use client";
+import styles from "./Home.module.css";
+import {Button, Card, Flex, Skeleton, Typography} from "antd"
+import Image from "next/image";
+import Link from "next/link";
+
+export default function Home() {
+    return (
+    <Card hoverable className={styles.card}>
+        <Flex justify="space-between">
+            <div className={styles.imageContainer}>
+                <Image src="/images/eu.jpg" alt="Meu nome" fill className={styles.image} />
+            </div>
+            <Flex vertical align="flex-end" justify="space-around">
+                <div>
+                    <Typography.Title level={3}>Página Inicial</Typography.Title>
+                    <Typography.Title level={5} type="sucess"></Typography.Title>
+                    <Typography.Paragraph>Sobre o Projeto:</Typography.Paragraph>
+                    <ul className={styles.list}>
+                        <li>Turma: 2TDS1</li>
+                        <li>Docentes: Thiago e Marcelo</li>
+                        <li>NEXT.JS FRONT-END 1</li>
+                        <p> Essa é uma atividade de Front-End, com o propósito de consumir uma API de Exames e Pacientes, fazendo com que Pacientes tenha relacionamento com Exames. Aqui eu uso as melhores práticas de desenvolvimento web.
+                        </p>     
+                    </ul>
+                    </div>
+                    <Link href="/pacientes" prefetch>
+                    <Button type="primary">Acessar minha Tabela Principal</Button>
+                    </Link>
+                </Flex>
+                </Flex>
+                </Card>
+    )
+}
